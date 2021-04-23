9e9c01bf5823af284af59c0bcb3dd833f6c662f4
Lucas340 <lucasstorres100@gmail.com>
2021-04-23 19:53:38 -0300

    feat: add the render route
    Add a route to render websites

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,5 @@ import { log } from '@utils/Logger';
 import app from './app';
 
 app.listen(3333, () => {
-  log('🚀 Server started on port 3333!');
+  log('🚀  Server started on port 3333!');
 });
---
+++ src/structures/Render.ts
@@ -0,0 +1,44 @@
+import puppeteer, { Page } from 'puppeteer';
+
+export default class Render {
+  private url: string;
+
+  private _page: Page | undefined;
+
+  constructor(url: string) {
+    this.url = url;
+  }
+
+  async render() {
+    const page = await this.getPage();
+
+    await page.setViewport({ width: 1200, height: 630 });
+    await page.goto(this.url, { waitUntil: 'load', timeout: 0 });
+
+    const screenshot = await page.screenshot({ type: 'jpeg' });
+
+    return screenshot;
+  }
+
+  private async getPage(): Promise<Page> {
+    if (this._page) {
+      return this._page;
+    }
+
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-gpu',
+        '--single-process',
+        '--disable-web-security',
+        '--disable-dev-profile',
+      ],
+    });
+
+    this._page = await browser.newPage();
+
+    return this._page;
+  }
+}
